6fb644c9f94d189122eefacde1727a6619d1e82c
burki864 <burakerenkisapro1122@gmail.com>
2025-12-30 20:07:47 +0300

    Update permissions.py

--- permissions.py
+++ permissions.py
@@ -1,7 +1,5 @@
 ADMINS = {
-    "burakerenkisapro1122@gmail.com": "123456",
-    "admin@gmail.com": "admin123"
+    "burakerenkisapro1122@gmail.com": "burki4509",
 }
-
 def check_admin(email, password):
     return ADMINS.get(email) == password
