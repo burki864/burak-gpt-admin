b64278a38aeb0cf97b110f085589e72e572607c1
burki864 <burakerenkisapro1122@gmail.com>
2025-12-30 20:02:25 +0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+import json
+
+USERS_FILE = "users.json"
+
+def load_users():
+    with open(USERS_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_users(data):
+    with open(USERS_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
